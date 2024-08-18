318591b05144fe689348e27c86cd60a785f18346
João Malvino Júnior <jmjr77@hotmail.com>
2024-08-18 18:23:54 -0300

    erro de provider

--- lib/App/modules/auth/login/login_page.dart
+++ lib/App/modules/auth/login/login_page.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list_minhas_contas/App/modules/auth/login/login_controller.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Provider.of<LoginController>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
-      body: Container(),
+      body: Container(
+        color: Colors.red,
+      ),
     );
   }
 }
--- lib/App/modules/splash/splash_page.dart
+++ lib/App/modules/splash/splash_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_minhas_contas/App/modules/auth/login/login_page.dart';
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const LoginPage();
   }
 }
