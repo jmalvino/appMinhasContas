cea9b8e4fa2a41b9eddafad2bf11711ccd18c6a2
João Malvino Júnior <jmalvino77@gmail.com>
2024-08-18 22:55:57 -0300

    corrigido, esqueci de envolver o provider no AppModule

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list_minhas_contas/App/modules/auth/login/login_controller.dart';
 import 'package:todo_list_minhas_contas/App/app_module.dart';
 
 void main() {
-  runApp(const AppModule());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => LoginController()),
+      ],
+      child: const AppModule(),
+    ),
+  );
 }
